1eba39bef439c0aef2f103e9128c76a08174bc24
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-04-09 14:05:46 +0900

    chore: 프로젝트 초기 설정 추가 (#1)

---
+++ src/main/java/com/teamflow/forestory_be/ForestoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.teamflow.forestory_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestoryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamflow/forestory_be/ForestoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamflow.forestory_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestoryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
