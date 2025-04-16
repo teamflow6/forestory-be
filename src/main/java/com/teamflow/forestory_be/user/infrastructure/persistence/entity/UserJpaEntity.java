11709ac6c9feabe99f789fa93705a41b4878129f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-04-16 18:25:00 +0900

    feat: 사용자 JPA 엔티티 클래스 추가 (#8)

---
+++ src/main/java/com/teamflow/forestory_be/user/infrastructure/persistence/entity/UserJpaEntity.java
@@ -0,0 +1,37 @@
+package com.teamflow.forestory_be.user.infrastructure.persistence.entity;
+
+import com.teamflow.forestory_be.support.common.domain.BaseTimeEntity;
+import com.teamflow.forestory_be.user.domain.vo.UserStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserJpaEntity extends BaseTimeEntity {
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+}
