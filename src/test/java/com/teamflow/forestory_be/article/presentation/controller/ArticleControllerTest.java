a9098183cdbadc912975dc729b232071228c34bc
이승주 <eun07213@gmail.com>
2025-04-28 17:44:12 +0900

    feat: 아티클 컨트롤러 테스트 코드 추가 (#11)

---
+++ src/test/java/com/teamflow/forestory_be/article/presentation/controller/ArticleControllerTest.java
@@ -0,0 +1,113 @@
+package com.teamflow.forestory_be.article.presentation.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamflow.forestory_be.article.application.dto.CreateArticleCommand;
+import com.teamflow.forestory_be.article.application.service.ArticleService;
+import com.teamflow.forestory_be.article.presentation.request.CreateArticleRequest;
+import com.teamflow.forestory_be.article.presentation.response.CreateArticleResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = ArticleController.class)
+class ArticleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ArticleService articleService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("아티클 등록 API - 성공")
+    void should_createArticle_success() throws Exception {
+        // given
+        CreateArticleRequest request = new CreateArticleRequest(
+            1L,
+            "제목",
+            "부제목",
+            "본문 내용",
+            "https://image.com/thumbnail.png"
+        );
+
+        given(articleService.create(new CreateArticleCommand(
+            request.authorId(),
+            request.title(),
+            request.subtitle(),
+            request.content(),
+            request.thumbnailUrl()
+        ))).willReturn(1L);
+
+        // when & then
+        mockMvc.perform(post("/api/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.articleId").value(1L));
+
+    }
+
+    @Test
+    @DisplayName("아티클 임시저장 API - 성공")
+    void should_draftArticle_success() throws Exception {
+        // given
+        CreateArticleRequest request = new CreateArticleRequest(
+            1L,
+            "제목",
+            "부제목",
+            "본문 내용",
+            "https://image.com/thumbnail.png"
+        );
+
+        given(articleService.draft(new CreateArticleCommand(
+            request.authorId(),
+            request.title(),
+            request.subtitle(),
+            request.content(),
+            request.thumbnailUrl()
+        ))).willReturn(2L);
+
+        // when & then
+        mockMvc.perform(post("/api/articles/draft")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.articleId").value(2L));
+
+    }
+
+    @Test
+    @DisplayName("아티클 등록 API - 실패(필수값누락)")
+    void should_createArticle_fail_missingField() throws Exception {
+        //given
+        CreateArticleRequest request = new CreateArticleRequest(
+            null,
+            "제목",
+            "부제목",
+            "본문 내용",
+            "https://image.com/thumbnail.png"
+
+        );
+
+        //when & then
+        mockMvc.perform(post("/api/articles")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isBadRequest());
+
+    }
+
+}
