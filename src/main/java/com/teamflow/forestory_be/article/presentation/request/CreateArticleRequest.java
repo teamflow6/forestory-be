ea577e15f29aea1e1dab9b005be08614c32bb319
이승주 <eun07213@gmail.com>
2025-04-28 17:44:46 +0900

    feat: 아티클 Request 추가 (#11)

--- src/main/java/com/teamflow/forestory_be/article/presentation/request/CreateArticleRequest.java
+++ src/main/java/com/teamflow/forestory_be/article/presentation/request/CreateArticleRequest.java
@@ -1,10 +1,22 @@
 package com.teamflow.forestory_be.article.presentation.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CreateArticleRequest(
+
+   @NotNull(message = "작성자 ID는 필수입니다.")
     Long authorId,
-    String title,
-    String subtitle,
-    String content,
-    String thumbnailUrl) {
+
+   @NotBlank(message = "제목은 필수입니다.")
+   String title,
+
+   @NotBlank(message = "부제목은 필수입니다.")
+   String subtitle,
+
+   @NotBlank(message = "본문은 필수입니다.")
+   String content,
+
+   String thumbnailUrl) {
 
 }
