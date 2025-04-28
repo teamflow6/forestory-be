69fa4aadac6764a216ac946d04fb8ba925b91ef1
이승주 <eun07213@gmail.com>
2025-04-28 15:21:44 +0900

    feat: 아티클 커맨드 추가 (#11)

---
+++ src/main/java/com/teamflow/forestory_be/article/application/dto/CreateArticleCommand.java
@@ -0,0 +1,11 @@
+package com.teamflow.forestory_be.article.application.dto;
+
+public record CreateArticleCommand (
+    Long authorId,
+    String title,
+    String subtitle,
+    String content,
+    String thumbnailUrl
+){
+
+}
