3c6567f87187685a6071035b69f16e7e6b329b71
이승주 <eun07213@gmail.com>
2025-04-29 15:08:43 +0900

    feat: 아티클 서비스 수정 (#11)

--- src/main/java/com/teamflow/forestory_be/article/application/service/ArticleService.java
+++ src/main/java/com/teamflow/forestory_be/article/application/service/ArticleService.java
@@ -6,8 +6,6 @@ import com.teamflow.forestory_be.article.domain.repository.ArticleRepositoryPort
 import com.teamflow.forestory_be.article.domain.vo.Content;
 import com.teamflow.forestory_be.article.domain.vo.Subtitle;
 import com.teamflow.forestory_be.article.domain.vo.Title;
-import com.teamflow.forestory_be.user.domain.entity.User;
-import com.teamflow.forestory_be.user.domain.repository.UserRepositoryPort;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,33 +14,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ArticleService {
 
-    private final UserRepositoryPort userRepositoryPort;
     private final ArticleRepositoryPort articleRepositoryPort;
 
-    private Article toArticle(CreateArticleCommand command, boolean isDraft) {
-        User author = userRepositoryPort.getById(command.authorId());
+    @Transactional
+    public Long create(CreateArticleCommand command) {
         Title title = new Title(command.title());
         Subtitle subtitle = new Subtitle(command.subtitle());
         Content content = new Content(command.content());
 
-        return isDraft
-            ? Article.draft(author, title, subtitle, content, command.thumbnailUrl())
-            : Article.create(author, title, subtitle, content, command.thumbnailUrl());
-    }
+        if (command.isDraft()) {
+            Article article = Article.draft(command.authorId(), title, subtitle, content, command.thumbnailUrl());
+            articleRepositoryPort.save(article);
+            return article.getId();
+        } else {
+            Article article = Article.create(command.authorId(), title, subtitle, content, command.thumbnailUrl());
+            articleRepositoryPort.save(article);
+            return article.getId();
+        }
 
-
-    @Transactional
-    public Long create(CreateArticleCommand command) {
-        Article article = toArticle(command, false);
-        articleRepositoryPort.save(article);
-        return article.getId();
-    }
-
-    @Transactional
-    public Long draft(CreateArticleCommand command) {
-        Article article = toArticle(command, true);
-        articleRepositoryPort.save(article);
-        return article.getId();
     }
 
 }
