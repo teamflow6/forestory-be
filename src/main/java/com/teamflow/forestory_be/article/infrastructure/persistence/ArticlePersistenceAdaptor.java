de9a87bdf594201584509a921c7632c4d824ef36
이승주 <eun07213@gmail.com>
2025-04-29 15:07:47 +0900

    feat: 아티클 영속성 어댑터 및매퍼 수정 (#11)

--- src/main/java/com/teamflow/forestory_be/article/infrastructure/persistence/ArticlePersistenceAdaptor.java
+++ src/main/java/com/teamflow/forestory_be/article/infrastructure/persistence/ArticlePersistenceAdaptor.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class ArticlePersistenceAdaptor implements ArticleRepositoryPort{
+public class ArticlePersistenceAdaptor implements ArticleRepositoryPort {
 
     private final ArticleJpaRepository articleJpaRepository;
 
--- src/main/java/com/teamflow/forestory_be/article/infrastructure/persistence/ArticlePersistenceMapper.java
+++ src/main/java/com/teamflow/forestory_be/article/infrastructure/persistence/ArticlePersistenceMapper.java
@@ -6,35 +6,34 @@ import com.teamflow.forestory_be.article.domain.vo.Subtitle;
 import com.teamflow.forestory_be.article.domain.vo.Title;
 import com.teamflow.forestory_be.article.infrastructure.persistence.entity.ArticleJpaEntity;
 
-import com.teamflow.forestory_be.user.domain.entity.User;
-import com.teamflow.forestory_be.user.infrastructure.persistence.UserPersistenceMapper;
 
 public class ArticlePersistenceMapper {
 
-    private ArticlePersistenceMapper() {}
+    private ArticlePersistenceMapper() {
+    }
 
-    public static Article toDomainEntity(ArticleJpaEntity articleJpaEntity){
-        User author = UserPersistenceMapper.toDomainEntity(articleJpaEntity.getAuthor());
+    public static Article toDomainEntity(ArticleJpaEntity articleJpaEntity) {
         return Article.reconstruct(
-            articleJpaEntity.getId(),
-            author,
-            new Title(articleJpaEntity.getTitle()),
-            new Subtitle(articleJpaEntity.getSubtitle()),
-            new Content(articleJpaEntity.getContent()),
-            articleJpaEntity.getThumbnailUrl(),
-            articleJpaEntity.getStatus()
+                articleJpaEntity.getId(),
+                articleJpaEntity.getAuthorId(),
+                new Title(articleJpaEntity.getTitle()),
+                new Subtitle(articleJpaEntity.getSubtitle()),
+                new Content(articleJpaEntity.getContent()),
+                articleJpaEntity.getThumbnailUrl(),
+                articleJpaEntity.getStatus()
         );
     }
-    public static ArticleJpaEntity toJpaEntity(Article article){
+
+    public static ArticleJpaEntity toJpaEntity(Article article) {
         return ArticleJpaEntity.builder()
-            .id(article.getId())
-            .author(UserPersistenceMapper.toJpaEntity(article.getAuthor()))
-            .title(article.getTitle().value())
-            .subtitle(article.getSubtitle().value())
-            .content(article.getContent().value())
-            .thumbnailUrl(article.getThumbnailUrl())
-            .status(article.getStatus())
-            .build();
+                .id(article.getId())
+                .authorId(article.getId())
+                .title(article.getTitle().value())
+                .subtitle(article.getSubtitle().value())
+                .content(article.getContent().value())
+                .thumbnailUrl(article.getThumbnailUrl())
+                .status(article.getStatus())
+                .build();
     }
 }
 
