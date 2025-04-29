24abc9fc6db5a1a8b8956df121367802d34e8d01
이승주 <eun07213@gmail.com>
2025-04-29 15:12:25 +0900

    feat: 아티클 생성용 명령 추가 (#11)

--- src/main/java/com/teamflow/forestory_be/article/application/dto/CreateArticleCommand.java
+++ src/main/java/com/teamflow/forestory_be/article/application/dto/CreateArticleCommand.java
@@ -1,11 +1,12 @@
 package com.teamflow.forestory_be.article.application.dto;
 
-public record CreateArticleCommand (
-    Long authorId,
-    String title,
-    String subtitle,
-    String content,
-    String thumbnailUrl
-){
+public record CreateArticleCommand(
+        Long authorId,
+        String title,
+        String subtitle,
+        String content,
+        String thumbnailUrl,
+        boolean isDraft
+) {
 
 }
