9082d891594d4ebee7ae518e2f3cbfee0366a1a5
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-04-29 19:54:59 +0900

    feat: 유저 온보딩, 이름 중복 검사 기능 추가 (#10)

--- src/main/java/com/teamflow/forestory_be/user/application/service/UserService.java
+++ src/main/java/com/teamflow/forestory_be/user/application/service/UserService.java
@@ -1,14 +1,17 @@
 package com.teamflow.forestory_be.user.application.service;
 
+import com.teamflow.forestory_be.user.application.dto.CheckNameExistsQuery;
 import com.teamflow.forestory_be.user.application.dto.CreateUserCommand;
+import com.teamflow.forestory_be.user.application.dto.OnboardingUserCommand;
 import com.teamflow.forestory_be.user.application.dto.ReadUserQuery;
 import com.teamflow.forestory_be.user.application.dto.UpdateUserCommand;
 import com.teamflow.forestory_be.user.domain.entity.User;
+import com.teamflow.forestory_be.user.domain.exception.UserNameDuplicatedException;
 import com.teamflow.forestory_be.user.domain.repository.UserRepositoryPort;
 import com.teamflow.forestory_be.user.domain.vo.Name;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,27 +20,52 @@ public class UserService {
     private final UserRepositoryPort userRepositoryPort;
 
     @Transactional
-    public void create(CreateUserCommand command) {
+    public Long create(CreateUserCommand command) {
         Name name = new Name(command.name());
         User user = User.create(
             name,
             command.profileImageUrl()
         );
-        userRepositoryPort.save(user);
+        return userRepositoryPort.save(user);
     }
 
     @Transactional
-    public void update(UpdateUserCommand command) {
+    public User completeOnboarding(OnboardingUserCommand command) {
         User user = userRepositoryPort.getById(command.userId());
         Name name = new Name(command.name());
-        User updatedUser = user.update(
-            name,
-            command.profileImageUrl()
-        );
+        validateNameDuplicated(name);
+
+        User updatedUser = user.completeOnboarding(command.userId(), name, command.profileImageUrl());
         userRepositoryPort.save(updatedUser);
+        return updatedUser;
+    }
+
+    @Transactional
+    public User update(UpdateUserCommand command) {
+        User user = userRepositoryPort.getById(command.userId());
+        Name name = new Name(command.name());
+
+        if (!name.equals(user.getName())) {
+            validateNameDuplicated(name);
+        }
+
+        User updatedUser = user.update(command.userId(), name, command.profileImageUrl());
+        userRepositoryPort.save(updatedUser);
+        return updatedUser;
     }
 
     public User getUserById(ReadUserQuery query) {
         return userRepositoryPort.getById(query.userId());
+    }
+
+    public boolean existsByName(CheckNameExistsQuery query) {
+        Name name = new Name(query.name());
+        return userRepositoryPort.existsByName(name);
+    }
+
+    private void validateNameDuplicated(Name name) {
+        if (userRepositoryPort.existsByName(name)) {
+            throw new UserNameDuplicatedException();
+        }
     }
 }
