7c71f94828e5a600078a3e5bf8d872b00b68ed94
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-05-03 20:34:02 +0900

    refactor: Spring Security 적용하지 않는 경로 수정 (#10)

--- src/main/java/com/teamflow/forestory_be/auth/infrastructure/security/SecurityIgnorePath.java
+++ src/main/java/com/teamflow/forestory_be/auth/infrastructure/security/SecurityIgnorePath.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 public final class SecurityIgnorePath {
@@ -16,11 +17,14 @@ public final class SecurityIgnorePath {
     private static OrRequestMatcher initIgnorePaths() {
         List<RequestMatcher> matchers = List.of(
             // User
-            new AntPathRequestMatcher("/api/v1/users/{userId}", HttpMethod.GET.name()),
+            new RegexRequestMatcher("/api/v1/users/\\d+$", HttpMethod.GET.name()),
             new AntPathRequestMatcher("/api/v1/users/name/exists", HttpMethod.GET.name()),
 
             // Auth
-            new AntPathRequestMatcher("/api/v1/auth/reissue", HttpMethod.POST.name())
+            new AntPathRequestMatcher("/api/v1/auth/reissue", HttpMethod.POST.name()),
+
+            // Article
+            new AntPathRequestMatcher("/api/v1/articles/**")
         );
         return new OrRequestMatcher(matchers);
     }
