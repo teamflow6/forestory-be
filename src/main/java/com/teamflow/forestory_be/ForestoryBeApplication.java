9d96e3884f83b346788f2dc8c125435dbbddff12
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-05-04 19:29:14 +0900

    refactor: JwtConfig 설정 클래스 추가 (#10)

--- src/main/java/com/teamflow/forestory_be/ForestoryBeApplication.java
+++ src/main/java/com/teamflow/forestory_be/ForestoryBeApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan
 public class ForestoryBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForestoryBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ForestoryBeApplication.class, args);
+    }
 
 }
