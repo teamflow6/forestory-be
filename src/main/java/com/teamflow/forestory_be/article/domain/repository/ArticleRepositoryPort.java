1786ad5b3480c342a634f1ceb796a3a8cbbb86fb
이승주 <eun07213@gmail.com>
2025-05-12 15:13:59 +0900

    feat: 아티클 영속성 가능 포트,어댑터 추가 (#19)

--- src/main/java/com/teamflow/forestory_be/article/domain/repository/ArticleRepositoryPort.java
+++ src/main/java/com/teamflow/forestory_be/article/domain/repository/ArticleRepositoryPort.java
@@ -1,11 +1,12 @@
 package com.teamflow.forestory_be.article.domain.repository;
 
 import com.teamflow.forestory_be.article.domain.entity.Article;
+import java.util.Optional;
 
 public interface ArticleRepositoryPort {
 
     void save(Article article);
 
-//    Article findById(Long id);
+    Article getById(Long id);
 //    List<Article> findByAuthorId(Long authorId);
 }
--- src/main/java/com/teamflow/forestory_be/article/infrastructure/persistence/ArticlePersistenceAdaptor.java
+++ src/main/java/com/teamflow/forestory_be/article/infrastructure/persistence/ArticlePersistenceAdaptor.java
@@ -4,6 +4,7 @@ import com.teamflow.forestory_be.article.domain.entity.Article;
 import com.teamflow.forestory_be.article.domain.repository.ArticleRepositoryPort;
 import com.teamflow.forestory_be.article.infrastructure.persistence.entity.ArticleJpaEntity;
 import com.teamflow.forestory_be.article.infrastructure.persistence.repository.ArticleJpaRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +18,13 @@ public class ArticlePersistenceAdaptor implements ArticleRepositoryPort {
     public void save(Article article) {
         ArticleJpaEntity articleJpaEntity = ArticlePersistenceMapper.toJpaEntity(article);
         articleJpaRepository.save(articleJpaEntity);
+    }
+
+    @Override
+    public Article getById(Long id) {
+        ArticleJpaEntity articleJpaEntity = articleJpaRepository.getById(id);
+        Article article = ArticlePersistenceMapper.toDomainEntity(articleJpaEntity);
+        return article;
     }
 
 
