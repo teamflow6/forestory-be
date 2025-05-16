12168364a014366233f453f5773bc6f6af94960f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-05-16 19:36:38 +0900

    feat: 헬스 체크용 컨트롤러 추가 (#30)

---
+++ src/main/java/com/teamflow/forestory_be/support/common/presentation/HealthController.java
@@ -0,0 +1,16 @@
+package com.teamflow.forestory_be.support.common.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK!");
+    }
+}
