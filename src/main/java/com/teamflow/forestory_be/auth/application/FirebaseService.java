0aff32bc4461191bb36593c9a08d2cd54b6180b4
jspark <98972385+JunseoParKK@users.noreply.github.com>
2025-06-06 14:38:18 +0900

    feat: Firebase 이메일 검증 로직 추가 (#13)

---
+++ src/main/java/com/teamflow/forestory_be/auth/application/FirebaseService.java
@@ -0,0 +1,36 @@
+package com.teamflow.forestory_be.auth.application;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.teamflow.forestory_be.auth.application.dto.FirebaseLoginCommand;
+import com.teamflow.forestory_be.auth.domain.exception.EmailNotVerifiedException;
+import com.teamflow.forestory_be.auth.domain.exception.InvalidTokenException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// TODO: 인프라 레이어로 분리
+@Service
+@RequiredArgsConstructor
+public class FirebaseService {
+
+    private final FirebaseApp firebaseApp;
+
+    public FirebaseLoginCommand verify(String idToken) {
+        try {
+            FirebaseToken firebaseToken = FirebaseAuth.getInstance(firebaseApp).verifyIdToken(idToken);
+
+            if (!firebaseToken.isEmailVerified()) {
+                throw new EmailNotVerifiedException();
+            }
+
+            return new FirebaseLoginCommand(
+                firebaseToken.getEmail(),
+                firebaseToken.getUid()
+            );
+        } catch (FirebaseAuthException ex) {
+            throw new InvalidTokenException();
+        }
+    }
+}
