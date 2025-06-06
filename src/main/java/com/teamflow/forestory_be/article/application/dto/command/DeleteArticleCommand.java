cb58c603055cb0f5029040b7b92168c75a3c7ff7
이승주 <eun07213@gmail.com>
2025-06-06 18:02:16 +0900

    feat: 아티클 삭제 command 필드 수정 (#35)

--- src/main/java/com/teamflow/forestory_be/article/application/dto/command/DeleteArticleCommand.java
+++ src/main/java/com/teamflow/forestory_be/article/application/dto/command/DeleteArticleCommand.java
@@ -2,6 +2,6 @@ package com.teamflow.forestory_be.article.application.dto.command;
 
 public record DeleteArticleCommand(
         Long articleId,
-        Long requesterId
+        Long authorId
 ) {
 }
