98df8e220656dd9c8a6a99cab5c9d6986752c26c
이승주 <eun07213@gmail.com>
2025-06-06 18:01:44 +0900

    feat: 아티클 상태 관련 메서드 추가 (#35)

--- src/main/java/com/teamflow/forestory_be/article/domain/vo/ArticleStatus.java
+++ src/main/java/com/teamflow/forestory_be/article/domain/vo/ArticleStatus.java
@@ -1,5 +1,15 @@
 package com.teamflow.forestory_be.article.domain.vo;
 
+import com.teamflow.forestory_be.article.domain.exception.InvalidArticleStatusException;
+
 public enum ArticleStatus {
-    DRAFT, PUBLISHED
+    DRAFT, PUBLISHED;
+
+    public static ArticleStatus from(String value) {
+        try {
+            return ArticleStatus.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw new InvalidArticleStatusException(value);
+        }
+    }
 }
