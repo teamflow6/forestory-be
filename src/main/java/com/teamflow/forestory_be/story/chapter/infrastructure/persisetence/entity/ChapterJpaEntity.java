19a8f6998382819e709bedfbc94a5b99d8421e2d
이승주 <eun07213@gmail.com>
2025-06-11 20:28:14 +0900

    feat: 챕터 jpa 엔티티 추가 (#14)

---
+++ src/main/java/com/teamflow/forestory_be/story/chapter/infrastructure/persisetence/entity/ChapterJpaEntity.java
@@ -0,0 +1,60 @@
+package com.teamflow.forestory_be.story.chapter.infrastructure.persisetence.entity;
+
+import com.teamflow.forestory_be.story.chapter.domain.vo.ChapterStatus;
+import com.teamflow.forestory_be.support.common.domain.BaseTimeEntity;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.units.qual.C;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "chapters")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ChapterJpaEntity extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "chapter_id")
+    private Long id;
+
+    @Column(name = "series_id", nullable = false)
+    private Long seriesId;
+
+    @Column(name = "author_id", nullable = false)
+    private Long authorId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "subtitle", nullable = false)
+    private String subtitle;
+
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @ElementCollection
+    @CollectionTable(name = "chapter_images", joinColumns = @JoinColumn(name = "chapter_id"))
+    @Column(name = "image_url")
+    private List<String> imageUrls;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ChapterStatus status;
+
+    @Column(name = "chapter_number", nullable = false, length = 10)
+    private String chapterNumber;
+}
