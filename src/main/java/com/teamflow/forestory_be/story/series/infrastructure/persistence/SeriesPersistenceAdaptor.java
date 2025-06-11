0a7bc84dfafe0b5b0dc3ce3b0ef20f8a5d85e291
이승주 <eun07213@gmail.com>
2025-06-11 20:33:55 +0900

    feat: 시리즈 영속성 어댑터 및 매퍼 추가 (#14)

---
+++ src/main/java/com/teamflow/forestory_be/story/series/infrastructure/persistence/SeriesPersistenceAdaptor.java
@@ -0,0 +1,21 @@
+package com.teamflow.forestory_be.story.series.infrastructure.persistence;
+
+import com.teamflow.forestory_be.story.series.domain.entity.Series;
+import com.teamflow.forestory_be.story.series.domain.repository.SeriesRepositoryPort;
+import com.teamflow.forestory_be.story.series.infrastructure.persistence.entity.SeriesJpaEntity;
+import com.teamflow.forestory_be.story.series.infrastructure.persistence.repository.SeriesJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SeriesPersistenceAdaptor implements SeriesRepositoryPort {
+
+    private final SeriesJpaRepository seriesJpaRepository;
+
+    @Override
+    public void save(Series series) {
+        SeriesJpaEntity seriesJpaEntity = SeriesPersistenceMapper.toJpaEntity(series);
+        seriesJpaRepository.save(seriesJpaEntity);
+    }
+}
