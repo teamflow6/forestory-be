91753a7bd4fcdc4b9cb0196f6b3daf476437e76e
이승주 <eun07213@gmail.com>
2025-07-07 00:29:19 +0900

    feat: Series controller update api 추가 (#53)

--- src/main/java/com/teamflow/forestory_be/story/series/presentation/controller/SeriesController.java
+++ src/main/java/com/teamflow/forestory_be/story/series/presentation/controller/SeriesController.java
@@ -1,13 +1,18 @@
 package com.teamflow.forestory_be.story.series.presentation.controller;
 
+import com.teamflow.forestory_be.article.presentation.dto.request.UpdateArticleRequest;
 import com.teamflow.forestory_be.story.series.application.dto.command.CreateSeriesCommand;
+import com.teamflow.forestory_be.story.series.application.dto.command.UpdateSeriesCommand;
 import com.teamflow.forestory_be.story.series.application.dto.query.GetSeriesQuery;
 import com.teamflow.forestory_be.story.series.application.service.SeriesService;
 import com.teamflow.forestory_be.story.series.domain.entity.Series;
 import com.teamflow.forestory_be.story.series.domain.vo.SeriesStatus;
+import com.teamflow.forestory_be.story.series.domain.vo.Type;
 import com.teamflow.forestory_be.story.series.presentation.dto.request.CreateSeriesRequest;
+import com.teamflow.forestory_be.story.series.presentation.dto.request.UpdateSeriesRequest;
 import com.teamflow.forestory_be.story.series.presentation.dto.response.CreateSeriesResponse;
 import com.teamflow.forestory_be.story.series.presentation.dto.response.GetSeriesResponse;
+import com.teamflow.forestory_be.story.series.presentation.dto.response.UpdateSeriesResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +22,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,4 +66,25 @@ public class SeriesController {
         GetSeriesResponse getSeriesResponse = seriesService.getSeries(query);
         return ResponseEntity.ok(getSeriesResponse);
     }
+
+    @PutMapping("/{seriesId}")
+    public ResponseEntity<UpdateSeriesResponse> updateSeries(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody @Valid UpdateSeriesRequest request,
+            @PathVariable @NotNull Long seriesId
+    ){
+        UpdateSeriesCommand updateSeriesCommand = new UpdateSeriesCommand(
+                seriesId,
+                userId,
+                request.title(),
+                request.introduction(),
+                request.thumbnailUrl(),
+                Type.from(request.type()),
+                SeriesStatus.from(request.seriesStatus())
+        );
+        UpdateSeriesResponse updateSeriesResponse = seriesService.update(updateSeriesCommand);
+        return ResponseEntity.ok(updateSeriesResponse);
+
+    }
+
 }
