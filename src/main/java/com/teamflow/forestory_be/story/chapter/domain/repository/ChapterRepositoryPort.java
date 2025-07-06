143bad8bdf0186d26fc889ed3ff157de2f08169a
이승주 <eun07213@gmail.com>
2025-07-06 14:40:40 +0900

    feat: ChapterRepositoryPort 추가 (#43)

--- src/main/java/com/teamflow/forestory_be/story/chapter/domain/repository/ChapterRepositoryPort.java
+++ src/main/java/com/teamflow/forestory_be/story/chapter/domain/repository/ChapterRepositoryPort.java
@@ -1,9 +1,16 @@
 package com.teamflow.forestory_be.story.chapter.domain.repository;
 
 import com.teamflow.forestory_be.story.chapter.domain.entity.Chapter;
+import com.teamflow.forestory_be.story.series.presentation.dto.response.ChapterWithCreatedAt;
+import java.util.List;
 
 public interface ChapterRepositoryPort {
     void save(Chapter chapter);
 
     String findLastChapterNumber(Long seriesId);
+
+    List<ChapterWithCreatedAt> findPublishedChaptersWithScroll(Long seriesId, String sort, String lastChapterNumber,
+                                                               int size);
+
+    Chapter getById(Long id);
 }
