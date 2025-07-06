24f3b782b9227e7ef6ac612ec51e9dcedcc7176d
이승주 <eun07213@gmail.com>
2025-07-06 14:45:00 +0900

    chore: 시리즈 컨트롤러 조회 api 추가 (#43)

--- src/main/java/com/teamflow/forestory_be/story/series/presentation/controller/SeriesController.java
+++ src/main/java/com/teamflow/forestory_be/story/series/presentation/controller/SeriesController.java
@@ -1,18 +1,25 @@
 package com.teamflow.forestory_be.story.series.presentation.controller;
 
 import com.teamflow.forestory_be.story.series.application.dto.command.CreateSeriesCommand;
+import com.teamflow.forestory_be.story.series.application.dto.query.GetSeriesQuery;
 import com.teamflow.forestory_be.story.series.application.service.SeriesService;
+import com.teamflow.forestory_be.story.series.domain.entity.Series;
 import com.teamflow.forestory_be.story.series.domain.vo.SeriesStatus;
 import com.teamflow.forestory_be.story.series.presentation.dto.request.CreateSeriesRequest;
 import com.teamflow.forestory_be.story.series.presentation.dto.response.CreateSeriesResponse;
+import com.teamflow.forestory_be.story.series.presentation.dto.response.GetSeriesResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,4 +48,16 @@ public class SeriesController {
 
     }
 
+    @GetMapping("/{seriesId}")
+    public ResponseEntity<GetSeriesResponse> getSeries(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable @NotNull Long seriesId,
+            @RequestParam(defaultValue = "asc") String sort, // asc: 첫화부터, desc: 최신순
+            @RequestParam(required = false) Integer lastChapterNumber,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        GetSeriesQuery query = new GetSeriesQuery(seriesId, sort, lastChapterNumber, size);
+        GetSeriesResponse getSeriesResponse = seriesService.getSeries(query);
+        return ResponseEntity.ok(getSeriesResponse);
+    }
 }
