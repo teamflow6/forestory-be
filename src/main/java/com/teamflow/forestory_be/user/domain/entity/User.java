4595d749d95a988b12cac0d9a673baf5e1896906
박준서 <wnstj444@naver.com>
2025-08-25 23:56:13 +0900

    refactor: 유저 도메인 엔티티 필드 VO로 변경 (#73)

--- src/main/java/com/teamflow/forestory_be/user/domain/entity/User.java
+++ src/main/java/com/teamflow/forestory_be/user/domain/entity/User.java
@@ -1,8 +1,10 @@
 package com.teamflow.forestory_be.user.domain.entity;
 
 import com.teamflow.forestory_be.support.common.util.TsidGenerator;
+import com.teamflow.forestory_be.user.domain.vo.ContactUrl;
 import com.teamflow.forestory_be.user.domain.vo.Introduction;
 import com.teamflow.forestory_be.user.domain.vo.Name;
+import com.teamflow.forestory_be.user.domain.vo.ProfileImageUrl;
 import com.teamflow.forestory_be.user.domain.vo.UserStatus;
 import java.util.Objects;
 import lombok.Getter;
@@ -12,61 +14,105 @@ public class User {
 
     private final Long id;
     private final Name name;
-    private final String profileImageUrl;
     private final UserStatus status;
     private final Introduction introduction;
+    private final ContactUrl contactUrl;
+    private final ProfileImageUrl profileImageUrl;
 
-    private User(Long id, Name name, String profileImageUrl, UserStatus status, Introduction introduction) {
+    private User(
+        Long id,
+        Name name,
+        UserStatus status,
+        ContactUrl contactUrl,
+        Introduction introduction,
+        ProfileImageUrl profileImageUrl
+    ) {
         this.id = Objects.requireNonNull(id);
         this.name = Objects.requireNonNull(name);
         this.profileImageUrl = profileImageUrl;
         this.status = Objects.requireNonNull(status);
         this.introduction = introduction; // <- 세팅(초기엔 null 가능)
+        this.contactUrl = contactUrl;
     }
 
-    public User completeOnboarding(Long userId, Name name, Introduction introduction, String profileImageUrl) {
+    public User completeOnboarding(Long userId, Name name, ProfileImageUrl profileImageUrl) {
         validateUser(userId);
         return new User(
-                this.id,
-                updateIfDifferent(name, this.name),
-                updateIfDifferent(profileImageUrl, this.profileImageUrl),
-                UserStatus.ACTIVE,
-                updateIfDifferent(introduction, this.introduction)
+            this.id,
+            updateIfDifferent(name, this.name),
+            UserStatus.ACTIVE,
+            ContactUrl.empty(),
+            Introduction.empty(),
+            updateIfDifferent(profileImageUrl, this.profileImageUrl)
         );
     }
 
-    public User update(Long userId, Name name, String profileImageUrl) {
+    public User update(
+        Long userId,
+        Name name,
+        Introduction introduction,
+        ProfileImageUrl profileImageUrl,
+        ContactUrl contactUrl
+    ) {
         validateUser(userId);
         validateStatus(UserStatus.ACTIVE);
         return new User(
-                this.id,
-                updateIfDifferent(name, this.name),
-                updateIfDifferent(profileImageUrl, this.profileImageUrl),
-                this.status,
-                this.introduction
+            this.id,
+            updateIfDifferent(name, this.name),
+            this.status,
+            updateIfDifferent(contactUrl, this.contactUrl),
+            updateIfDifferent(introduction, this.introduction),
+            updateIfDifferent(profileImageUrl, this.profileImageUrl)
         );
     }
 
-    /** 소개 변경 */
+    public boolean isSameName(Name name) {
+        return this.name.equals(name);
+    }
+
+    /**
+     * 소개 변경
+     */
     public User updateIntroduction(Long userId, Introduction introduction) {
         validateUser(userId);
         // 필요 시 상태 검증 추가: validateStatus(UserStatus.ACTIVE);
         return new User(
-                this.id,
-                this.name,
-                this.profileImageUrl,
-                this.status,
-                updateIfDifferent(introduction, this.introduction)
+            this.id,
+            this.name,
+            this.status,
+            this.contactUrl,
+            updateIfDifferent(introduction, this.introduction),
+            this.profileImageUrl
         );
     }
 
-    public static User create(Name name, String profileImageUrl) {
+    public static User create(Name name, ProfileImageUrl profileImageUrl) {
         Long id = TsidGenerator.generate();
-        return new User(id, name, profileImageUrl, UserStatus.ONBOARDING, new Introduction(""));
+        return new User(
+            id,
+            name,
+            UserStatus.ONBOARDING,
+            ContactUrl.empty(),
+            Introduction.empty(),
+            profileImageUrl
+        );
     }
 
-    public static User reconstruct(Long id, Name name, String profileImageUrl, UserStatus status, Introduction introduction) {
-        return new User(id, name, profileImageUrl, status, introduction != null ? introduction : new Introduction(""));
+    public static User reconstruct(
+        Long id, Name name,
+        ProfileImageUrl profileImageUrl,
+        UserStatus status,
+        Introduction introduction,
+        ContactUrl contactUrl
+    ) {
+        return new User(
+            id,
+            name,
+            status,
+            contactUrl,
+            introduction,
+            profileImageUrl
+        );
     }
 
     private void validateUser(Long userId) {
@@ -82,7 +128,9 @@ public class User {
     }
 
     private static <T> T updateIfDifferent(T newValue, T currentValue) {
-        if (newValue == null || newValue.equals(currentValue)) return currentValue;
+        if (newValue == null || newValue.equals(currentValue)) {
+            return currentValue;
+        }
         return newValue;
     }
 }
